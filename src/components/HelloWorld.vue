ceb7885c46cd7479c1e0465d23bcc70a1a477302
Renato Rosa Franco <renato.franco@asisit.com.br>
2018-10-20 15:27:04 -0300

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="hello">
+		<h1 :title='msg'>{{ msg }}</h1>
+	</div>
+</template>
+
+<style type="text/css">
+	
+</style>
+
+<script type="text/javascript">
+	export default {
+		data() {
+			return {
+				msg: 'Bem vindo a sua primeira aplicação com Vue.js'
+			}
+		}
+	}
+</script>
